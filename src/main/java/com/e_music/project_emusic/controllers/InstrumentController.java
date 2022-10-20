0264d257da14f6edff681ac82983a9ef53db9b44
Matias Salomone <matisalomone@hotmail.com>
2022-10-20 16:19:08 -0300

    :D

--- src/main/java/com/e_music/project_emusic/controllers/InstrumentController.java
+++ src/main/java/com/e_music/project_emusic/controllers/InstrumentController.java
@@ -3,6 +3,7 @@ package com.e_music.project_emusic.controllers;
 import com.e_music.project_emusic.entities.Instrument;
 import com.e_music.project_emusic.services.ServiceInstrument;
 import com.e_music.project_emusic.services.ServiceInstrumentImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,21 +12,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "/emusic/instruments")
-public class InstrumentController extends BaseControllerImpl< Instrument, ServiceInstrumentImpl >{
+public class InstrumentController extends BaseControllerImpl<Instrument, ServiceInstrumentImpl> {
 
 
-    @GetMapping ( value = "/start" )
-    public String start( Model model ) {
-        try {
-            return "views/start";
-        } catch ( Exception e ) {
-            model.addAttribute( "error", e.getMessage( ) );
-            return "error";
-        }
+  @GetMapping(value = "/start")
+  public ModelAndView start(Model model) {
+    ModelAndView modelAndView = new ModelAndView();
+    try {
+      modelAndView.setViewName("views/start.html");
+    } catch (Exception e) {
+      log.info(e.getMessage(),e) ;
+      modelAndView.setViewName("error.html");
     }
+    return modelAndView;
+  }
 
 }
