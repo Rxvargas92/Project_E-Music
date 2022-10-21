25c125112365e00c645182bccc38e8b87890c172
Araya Alejo <103607905+Araya-Alejo@users.noreply.github.com>
2022-10-21 11:05:33 -0300

    add /register

--- src/main/java/com/e_music/project_emusic/controllers/UserController.java
+++ src/main/java/com/e_music/project_emusic/controllers/UserController.java
@@ -2,13 +2,30 @@ package com.e_music.project_emusic.controllers;
 
 import com.e_music.project_emusic.entities.User;
 import com.e_music.project_emusic.services.ServiceUserImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "/emusic/users")
 public class UserController extends BaseControllerImpl<User, ServiceUserImpl>{
+
+    @GetMapping (value = "/register")
+    public ModelAndView register( Model model) {
+        ModelAndView modelAndView = new ModelAndView();
+        try {
+            modelAndView.setViewName("views/register.html");
+        } catch (Exception e) {
+            log.info(e.getMessage(),e) ;
+            modelAndView.setViewName("error.html");
+        }
+        return modelAndView;
+    }
 
 }
