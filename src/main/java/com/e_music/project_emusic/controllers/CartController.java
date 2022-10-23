48b5a0a466ec99a097dce82296228f1336d46807
Araya Alejo <103607905+Araya-Alejo@users.noreply.github.com>
2022-10-23 10:01:04 -0300

    add font awesome and create the file of cart.html

--- src/main/java/com/e_music/project_emusic/controllers/CartController.java
+++ src/main/java/com/e_music/project_emusic/controllers/CartController.java
@@ -2,12 +2,30 @@ package com.e_music.project_emusic.controllers;
 
 import com.e_music.project_emusic.entities.Cart;
 import com.e_music.project_emusic.services.ServiceCartImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "/emusic/carts")
 public class CartController extends BaseControllerImpl<Cart, ServiceCartImpl> {
+
+    @GetMapping (value = "/cart")
+    public ModelAndView cart( Model model) {
+        ModelAndView modelAndView = new ModelAndView();
+        try {
+            modelAndView.setViewName("views/cart.html");
+        } catch (Exception e) {
+            log.info(e.getMessage(),e) ;
+            modelAndView.setViewName("error.html");
+        }
+        return modelAndView;
+    }
+
 }
