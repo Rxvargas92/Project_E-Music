048a701976910fa9b19596fbef4311e1ee179bd3
Araya Alejo <103607905+Araya-Alejo@users.noreply.github.com>
2022-10-31 14:59:22 -0300

    update UserCategory

--- src/main/java/com/e_music/project_emusic/repositories/RepositoryUser.java
+++ src/main/java/com/e_music/project_emusic/repositories/RepositoryUser.java
@@ -1,10 +1,16 @@
 package com.e_music.project_emusic.repositories;
 
+import com.e_music.project_emusic.entities.Instrument;
 import com.e_music.project_emusic.entities.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RepositoryUser extends RepositoryBase<User, Long> {
-    public User findByEmail(String email);
+
+    @Query ( value = "SELECT * FROM user WHERE user.email = :email", nativeQuery = true )
+    User findByEmail(String email);
 
 }
