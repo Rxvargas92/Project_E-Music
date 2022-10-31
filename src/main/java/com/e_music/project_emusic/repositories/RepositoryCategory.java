0d50b8ece2e6291d27d3d45990de1c9e9a891a2f
Araya Alejo <103607905+Araya-Alejo@users.noreply.github.com>
2022-10-31 14:59:35 -0300

    update CategoryRepository

--- src/main/java/com/e_music/project_emusic/repositories/RepositoryCategory.java
+++ src/main/java/com/e_music/project_emusic/repositories/RepositoryCategory.java
@@ -2,8 +2,16 @@ package com.e_music.project_emusic.repositories;
 
 
 import com.e_music.project_emusic.entities.Category;
+import com.e_music.project_emusic.entities.Instrument;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RepositoryCategory extends RepositoryBase<Category, Long> {
+
+    @Query ( value = "SELECT * FROM category WHERE category.active = true", nativeQuery = true )
+    List< Category > findAllByActive( );
+
 }
