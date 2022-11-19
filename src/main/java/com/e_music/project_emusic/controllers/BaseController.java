698eddc5a26acd22bf400a0cbadb7207f79b4ed9
Rxvargas92 <rodrigox.vargas@alumnos.frm.utn.edu.ar>
2022-11-19 17:40:34 -0300

    seguridad

--- src/main/java/com/e_music/project_emusic/controllers/BaseController.java
+++ src/main/java/com/e_music/project_emusic/controllers/BaseController.java
@@ -3,6 +3,7 @@ package com.e_music.project_emusic.controllers;
 import com.e_music.project_emusic.entities.abstractions.Base;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -10,10 +11,17 @@ import java.io.Serializable;
 
 public interface BaseController <E extends Base, ID extends Serializable> {
 
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> getAll();
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> getAll(Pageable pageable);
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> getOne(@PathVariable ID id);
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> saveOne(@RequestBody E entity) throws Exception;
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> updateOne(@PathVariable ID id, @RequestBody E entity);
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> deleteById(@PathVariable ID id);
+    
 }
